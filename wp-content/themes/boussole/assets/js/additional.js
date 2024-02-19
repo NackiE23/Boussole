9f11f8a82b8d70111b52c7c575b5d3009213433a
NackiE23 <dimavarchuc@gmail.com>
2024-02-19 10:53:04 +0200

    Fix media adaptation

--- wp-content/themes/boussole/assets/js/additional.js
+++ wp-content/themes/boussole/assets/js/additional.js
@@ -2,12 +2,6 @@ $(document).ready(function() {
     $("#menu-opener").on("click", function() {
         $("#menu-opener").toggleClass("active");
         $("#full-page-menu").toggleClass("active");
-
-        if (window.innerHeight > 734) {
-            $('#full-page-menu .inner').css('justify-content', 'center');
-        } else {
-            $('#full-page-menu .inner').css('justify-content', 'normal');
-        }
     })
 
     $("#full-page-menu .inner").on("click", '*', function() {
